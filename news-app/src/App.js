84d8ed95fff2e1e3b3860fdedb724949205f3109
willbyrne <will.bl.byrne@gmail.com>
2023-02-07 18:08:08 +0000

    category top headline selector in place & search bar working. Reset functions also working

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -6,45 +6,55 @@ import SearchResults from "./components/SearchResults/SearchResults";
 import useFetch from "./hooks/useFetch/useFetch";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [searchCategory, setSearchCategory] = useState("general");
-  const [searchLanguage, setSearchLanguage] = useState("en");
-
   //environment variable for security
   const apiKey = process.env.REACT_APP_API_KEY;
+  // initial top headline fetch url for page load
   const urlOnLoad =
-    "https://gnews.io/api/v4/top-headlines?category=" +
-    searchCategory +
+    "https://gnews.io/api/v4/top-headlines?category=general" +
     "&apikey=" +
     apiKey +
-    "&lang=" +
-    searchLanguage +
+    "&lang=en" +
     "&max=10";
   const [url, setUrl] = useState(urlOnLoad);
 
   //custom hook for fetching news from API
   const { data, error } = useFetch(url);
 
-  async function getCustomNews() {
-    let urlCustomSearch =
+  function getCustomNews(searchTerm) {
+    let url =
       "https://gnews.io/api/v4/search?q=" +
       searchTerm +
       "&apikey=" +
       apiKey +
-      "&lang=" +
-      searchLanguage +
+      "&lang=en" +
       "&max=10";
-    setUrl(urlCustomSearch);
-    console.log(url);
+    setUrl(url);
   }
+
+  function searchByCategory(category) {
+    let url =
+      "https://gnews.io/api/v4/top-headlines?category=" +
+      category +
+      "&apikey=" +
+      apiKey +
+      "&lang=en" +
+      "&max=10";
+    setUrl(url);
+  }
+
+  function resetSearchResults() {
+    setUrl(urlOnLoad);
+  }
+
   return (
     <div className="app-container">
       <header className="app-header"></header>
       <div className="body-container">
         <div className="search-container">
           <SearchBar
-            setSearchTerm={setSearchTerm}
             getCustomNews={getCustomNews}
+            resetSearchResults={resetSearchResults}
+            searchByCategory={searchByCategory}
           />
           <SearchResults data={data} error={error} />
         </div>
--- news-app/src/components/SearchBar.js/SearchBar.js
+++ news-app/src/components/SearchBar.js/SearchBar.js
@@ -1,19 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function SearchBar({ setSearchTerm, getCustomNews }) {
-  function handleChange(e) {
-    setSearchTerm(e.target.value);
+export default function SearchBar({
+  getCustomNews,
+  resetSearchResults,
+  searchByCategory,
+}) {
+  // const [category, setCategory] = useState(null);
+  const [searchTerm, setSearchTerm] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    getCustomNews(searchTerm);
   }
 
-  function handleClick() {
-    console.log("click");
-    getCustomNews();
+  function handleCategoryChange(e) {
+    searchByCategory(e.target.value);
+  }
+  function handleReset() {
+    resetSearchResults();
+    setSearchTerm("");
   }
 
   return (
     <div className="search-bar-container">
-      <input placeholder="Search for the latest news" onChange={handleChange} />
-      <button onClick={handleClick}>Search</button>
+      <form onSubmit={handleSubmit}>
+        <label for="searchCategory">
+          See the latest in different topics:
+          <select
+            name="searchCategory"
+            defaultValue="category"
+            onChange={handleCategoryChange}
+          >
+            <option>Category</option>
+            <option value="general">General</option>
+            <option value="world">World</option>
+            <option value="business">Business</option>
+            <option value="technology">Technology</option>
+            <option value="entertainment">Entertainment</option>
+            <option value="sports">Sports</option>
+            <option value="science">Science</option>
+            <option value="Health">Health</option>
+          </select>
+        </label>
+        <label>Or search for articles on specific topic</label>
+        <input
+          placeholder="Search for the latest news"
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        />
+        <button type="submit">Search</button>
+        <button type="reset" onClick={handleReset}>
+          Reset
+        </button>
+      </form>
     </div>
   );
 }
