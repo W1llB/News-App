5a48a3114e9eef6a49e01996e973d91905ee5212
willbyrne <will.bl.byrne@gmail.com>
2023-02-07 13:18:24 +0000

    more css - cards mvp done

--- news-app/src/components/ArticleCard/ArticleCard.js
+++ news-app/src/components/ArticleCard/ArticleCard.js
@@ -1,15 +1,42 @@
 import React from "react";
 import "./ArticleCard.css";
+import moment from "moment";
 
 export default function ArticleCard({ article }) {
+  //calculate time since publishing
+  const now = moment(article.publishedAt).fromNow();
   return (
     <div className="article-card">
-      <div className="article-title">
-        <h3>{article.title}</h3>
+      <div className="article-content">
+        <div className="article-publisher">
+          <a
+            style={{ textDecoration: "none" }}
+            href={article.source.url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {article.source.name}
+          </a>
+        </div>
+        <div className="article-title">
+          <h3>{article.title}</h3>
+        </div>
         <p className="article-description">{article.description}</p>
-        <a className="article-url" href={article.url}>
-          Go to article
-        </a>
+        <div className="article-footer">
+          <p>{now}</p>
+
+          <a
+            className="article-url"
+            style={{ textDecoration: "none" }}
+            href={article.url}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Go to article
+          </a>
+
+          <div />
+        </div>
       </div>
       <img src={article.image} alt="article related." />
     </div>
